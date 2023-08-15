b843dc4d85002d53c5527a26470061b36a1563cd
hadar cohen <hadarco158@gmail.com>
2023-08-15 10:20:33 +0300

    Create GitHubFile.java

---
+++ src/GitHubFile.java
@@ -0,0 +1,11 @@
+public class GitHubFile {
+    public static void main(String[] args) {
+        System.out.println("hello, world!1");
+        System.out.print("hello, world!2");
+        System.out.println("hello, world!3");
+        System.out.println("hello, world!4\n");
+        System.out.print("\thello, world!5");
+    }
+
+
+}
